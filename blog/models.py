a65a788bd1bf4264fc7b430ee39a0a6103839d81
wanje125 <whdhkswp07@naver.com>
2021-01-22 11:51:36 +0900

    모델을 생성하고 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title=models.CharField(max_length=30, verbose_name='제목')
+    content=models.TextField(verbose_name='내용')
+    created_at=models.DateTimeField(auto_now_add=True,verbose_name='생성시간')
+    updated_at=models.DateTimeField(auto_now=True,verbose_name='변경시간')
+
+    def __str__(self):
+        return f'[{self.pk}{self.title}]'
+
