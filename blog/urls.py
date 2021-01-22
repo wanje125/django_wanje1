227364ef76305cf1cc591f8046924154d4bf0d8b
wanje125 <whdhkswp07@naver.com>
2021-01-22 14:45:15 +0900

    CBV로 블로그 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.PostList.as_view())
+    path('',views.PostList.as_view()),
+    path('<int:pk>/',views.PostDetail.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
@@ -7,3 +7,7 @@ class PostList(ListView):
     model=Post
     template_name='blog/index.html'
     ordering='-pk'
+
+class PostDetail(DetailView):
+    model=Post
+    template_name='single_pages/landing.html'
