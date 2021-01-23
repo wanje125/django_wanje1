d24e5ff62eb6bf185d123d7531893280bd21ec69
wanje125 <whdhkswp07@naver.com>
2021-01-23 15:08:52 +0900

    포스트 상세 페이지 이미지 출력

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     content=models.TextField(verbose_name='내용')
     created_at=models.DateTimeField(auto_now_add=True,verbose_name='생성시간')
     updated_at=models.DateTimeField(auto_now=True,verbose_name='변경시간')
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'[{self.pk}{self.title}]'
