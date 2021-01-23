e4d9ce2be6535c762f2b052cbabfd0acdaf1cbab
wanje125 <whdhkswp07@naver.com>
2021-01-23 20:25:38 +0900

    미리보기와 요약문 추가

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ import os
 class Post(models.Model):
     title=models.CharField(max_length=30, verbose_name='제목')
     content=models.TextField(verbose_name='내용')
+    hook_text=models.CharField(max_length=100,blank=True, verbose_name='요약문')
     created_at=models.DateTimeField(auto_now_add=True,verbose_name='생성시간')
     updated_at=models.DateTimeField(auto_now=True,verbose_name='변경시간')
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
