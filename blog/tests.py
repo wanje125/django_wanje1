bc744d68b5d446ac43074d30709c5ee386f2de4b
wanje125 <whdhkswp07@naver.com>
2021-01-24 13:11:44 +0900

    포스트 목록 페이지와 포스트 상세페이지 디자인 통일

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client #Client 추가
+from bs4 import BeautifulSoup
+from .models import Post
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client=Client()
+
+    def test_post_list(self):
+        response=self.client.get('/blog/')
+        self.assertEqual(response.status_code,200)
+        soup=BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(soup.title.text,'Blog')
+        navbar=soup.nav
+        self.assertIn('Blog',navbar.text)
+        self.assertIn('About Me',navbar.text)
+        self.assertEqual(Post.objects.count(),0)
+        #귀찮으니까 나중에 공부하자.
+
