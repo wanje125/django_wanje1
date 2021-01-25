a282e1f48e0e00dfd1095825ead23ae23d8504d9
wanje125 <whdhkswp07@naver.com>
2021-01-25 12:09:34 +0900

    포스트 목록 페이지에 카테고리 기능 추가하기

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 # Create your models here.
+
+class Category(models.Model): #post 위에 추가 해야된다
+    name=models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural='Categories'
+
 class Post(models.Model):
     title=models.CharField(max_length=30, verbose_name='제목')
     content=models.TextField(verbose_name='내용')
@@ -11,6 +22,7 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
 
     def __str__(self):
@@ -24,4 +36,6 @@ class Post(models.Model):
     
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 # Create your views here.
 
 class PostList(ListView):
@@ -8,6 +8,18 @@ class PostList(ListView):
     template_name='blog/blog_list.html'
     ordering='-pk'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList,self).get_context_data()
+        context['categories']=Category.objects.all()
+        context['no_category_post_count']=Post.objects.filter(category=None).count()
+        return context
+
 class PostDetail(DetailView):
     model=Post
     template_name='blog/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail,self).get_context_data()
+        context['categories']=Category.objects.all()
+        context['no_category_post_count']=Post.objects.filter(category=None).count()
+        return context
