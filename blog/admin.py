fe8437cf2968d66a98f063bce77fb1ec38ee6cf1
wanje125 <whdhkswp07@naver.com>
2021-01-25 21:58:14 +0900

    포스트 목록 페이지와 상세페이지에 태그기능 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Tag
 # Register your models here.
 admin.site.register(Post)
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name',)} #admin에서 카테고리의 name을 생성했을때 자동으로 같은 이름의 slug 생성
 
 admin.site.register(Category, CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)} #admin에서 카테고리의 name을 생성했을때 자동으로 같은 이름의 slug 생성
+
+admin.site.register(Tag,TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 # Create your models here.
+class Tag(models.Model): #post 위에 추가 해야된다
+    name=models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/' #f string이란 방법인데 format이랑 똑같다.
 
 class Category(models.Model): #post 위에 추가 해야된다
     name=models.CharField(max_length=50, unique=True)
@@ -27,6 +36,7 @@ class Post(models.Model):
     file_upload = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    tags=models.ManyToManyField(Tag, blank=True)
 
 
     def __str__(self):
