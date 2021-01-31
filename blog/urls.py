c1d4a3d985a554e82116c6c23b992b3c18a3d7d0
wanje125 <whdhkswp07@naver.com>
2021-01-31 11:23:45 +0900

    댓글 삭제기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('create_post/',views.PostCreate.as_view()),
     path('update_post/<int:pk>/',views.PostUpdate.as_view()),
     path('<int:pk>/new_comment/',views.new_comment),
-    path('update_comment/<int:pk>/',views.CommentUpdate.as_view())
+    path('update_comment/<int:pk>/',views.CommentUpdate.as_view()),
+    path('delete_comment/<int:pk>/',views.delete_comment)
 ]
